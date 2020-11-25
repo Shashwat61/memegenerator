57e41beb5129c96563a1b956db095f68ef0d019e
Shashwat61 <68372070+Shashwat61@users.noreply.github.com>
2020-11-25 19:16:18 +0530

    moving index file outside  src folder

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "Header.js";
+import Meme from "Memegenerator.js";
+
+import "../public/styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Meme />
+    </div>
+  );
+}
+export default App;
